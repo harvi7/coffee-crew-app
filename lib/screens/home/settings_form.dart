44a4da6a8e83d57d774319529f8efbe55b431291
Harsh Virani <iamharshvirani7@gmail.com>
2020-04-18 19:36:14 -0700

    Added Size preference

---
+++ lib/screens/home/settings_form.dart
@@ -0,0 +1,114 @@
+import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
+import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SettingsForm extends StatefulWidget {
+  @override
+  _SettingsFormState createState() => _SettingsFormState();
+}
+
+class _SettingsFormState extends State<SettingsForm> {
+
+  final _formKey = GlobalKey<FormState>();
+  final List<String> sugars = ['0', '1', '2', '3', '4'];
+  final List<int> strengths = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+  final List<String> sizes = ['Tall', 'Grande', 'Venti'];
+
+  // form values
+  String _currentName;
+  String _currentSugars;
+  int _currentStrength;
+  String _currentSize;
+
+  @override
+  Widget build(BuildContext context) {
+
+    User user = Provider.of<User>(context);
+
+    return StreamBuilder<UserData>(
+      stream: DatabaseService(uid: user.uid).userData,
+      builder: (context, snapshot) {
+        if(snapshot.hasData){
+          UserData userData = snapshot.data;
+          return Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Update your brew settings.',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                SizedBox(height: 15.0),
+                TextFormField(
+                  initialValue: userData.name,
+                  decoration: textInputDecoration,
+                  validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+                  onChanged: (val) => setState(() => _currentName = val),
+                ),
+                SizedBox(height: 5.0),
+                DropdownButtonFormField(
+                  value: _currentSugars ?? userData.sugars,
+                  decoration: textInputDecoration,
+                  items: sugars.map((sugar) {
+                    return DropdownMenuItem(
+                      value: sugar,
+                      child: Text('$sugar sugars'),
+                    );
+                  }).toList(),
+                  onChanged: (val) => setState(() => _currentSugars = val ),
+                ),
+                SizedBox(height: 5.0),
+
+                // Size Dropdown
+                DropdownButtonFormField(
+                  value: _currentSugars ?? userData.size,
+                  decoration: textInputDecoration,
+                  items: sizes.map((size) {
+                    return DropdownMenuItem(
+                      value: size,
+                      child: Text('$size'),
+                    );
+                  }).toList(),
+                  onChanged: (val) => setState(() => _currentSize = val ),
+                ),
+                SizedBox(height: 10.0),
+                Slider(
+                  value: (_currentStrength ?? userData.strength).toDouble(),
+                  activeColor: Colors.brown[_currentStrength ?? userData.strength],
+                  inactiveColor: Colors.brown[_currentStrength ?? userData.strength],
+                  min: 100.0,
+                  max: 900.0,
+                  divisions: 8,
+                  onChanged: (val) => setState(() => _currentStrength = val.round()),
+                ),
+                RaisedButton(
+                  color: Colors.pink[400],
+                  child: Text(
+                    'Update',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if(_formKey.currentState.validate()){
+                      await DatabaseService(uid: user.uid).updateUserData(
+                        _currentSugars ?? snapshot.data.sugars, 
+                        _currentName ?? snapshot.data.name, 
+                        _currentStrength ?? snapshot.data.strength,
+                        _currentSize ?? snapshot.data.size
+                      );
+                      Navigator.pop(context);
+                    }
+                  }
+                ),
+              ],
+            ),
+          );
+        } else {
+          return Loading();
+        }
+      }
+    );
+  }
+}
---
+++ lib/services/database.dart
@@ -0,0 +1,58 @@
+import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/models/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+
+  final String uid;
+  DatabaseService({ this.uid });
+
+  // collection reference
+  final CollectionReference brewCollection = Firestore.instance.collection('brews');
+
+  Future<void> updateUserData(String sugars, String name, int strength, String size) async {
+    return await brewCollection.document(uid).setData({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength,
+      'size': size
+    });
+  }
+
+  // brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc){
+      //print(doc.data);
+      return Brew(
+        name: doc.data['name'] ?? '',
+        strength: doc.data['strength'] ?? 0,
+        sugars: doc.data['sugars'] ?? '0',
+        size: doc.data['size'] ?? 'Tall'
+      );
+    }).toList();
+  }
+
+  // user data from snapshots
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+      uid: uid,
+      name: snapshot.data['name'],
+      sugars: snapshot.data['sugars'],
+      strength: snapshot.data['strength'],
+      size: snapshot.data['size']
+    );
+  }
+
+  // get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots()
+      .map(_brewListFromSnapshot);
+  }
+
+  // get user doc stream
+  Stream<UserData> get userData {
+    return brewCollection.document(uid).snapshots()
+      .map(_userDataFromSnapshot);
+  }
+
+}
